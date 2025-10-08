631684e0f27561b7e73730b55a89e452df54e373
abuyidaries <abuyidaries6@gmail.com>
2025-10-08 04:33:52 +0400

    handled cart and checkout page for logged in user

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -6,12 +6,19 @@ def counter(request):
     cart_count = 0
     if 'admin' in request.path:
         return {}
-    else:
-        try:
-            cart = Cart.objects.filter(cart_id=_cart_id(request))
-            cart_items = CartItem.objects.all().filter(cart=cart[:1])
-            for cart_item in cart_items:
-                cart_count += cart_item.quantity
-        except Cart.DoesNotExist:
-            cart_count = 0
+    try:
+        if request.user.is_authenticated:
+            # Logged-in users: filter by user
+            cart_items = CartItem.objects.filter(user=request.user)
+        else:
+            # Guests: filter by cart ID
+            cart = Cart.objects.filter(cart_id=_cart_id(request)).first()
+            if cart:
+                cart_items = CartItem.objects.filter(cart=cart)
+            else:
+                cart_items = []
+        for cart_item in cart_items:
+            cart_count += cart_item.quantity
+    except Cart.DoesNotExist:
+        cart_count = 0
     return dict(cart_count=cart_count)
